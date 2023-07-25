335b05282cd6d594142bff40453151044de18e57
Cbgogogog <1293543528@qq.com>
2023-07-26 03:04:19 +0800

    feat: 配置化jwtKey,在feign中增加getUseridByName

--- src/main/java/com/ps/utils/JwtUtils.java
+++ src/main/java/com/ps/utils/JwtUtils.java
@@ -3,21 +3,18 @@ package com.ps.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
 import java.util.Map;
 
 public class JwtUtils {
-
-    private static String signKey = "4d15w6sa4wd12da";
-    private static Long expire = 43200000L;
-
     /**
      * 生成JWT令牌
      * @param claims JWT第二部分负载 payload 中存储的内容
      * @return
      */
-    public static String generateJwt(Map<String, Object> claims){
+    public static String generateJwt(Map<String, Object> claims, String signKey, Long expire){
         String jwt = Jwts.builder()
                 .addClaims(claims)
                 .signWith(SignatureAlgorithm.HS256, signKey)
@@ -31,7 +28,7 @@ public class JwtUtils {
      * @param jwt JWT令牌
      * @return JWT第二部分负载 payload 中存储的内容
      */
-    public static Claims parseJWT(String jwt){
+    public static Claims parseJWT(String jwt, String signKey){
         Claims claims = Jwts.parser()
                 .setSigningKey(signKey)
                 .parseClaimsJws(jwt)
