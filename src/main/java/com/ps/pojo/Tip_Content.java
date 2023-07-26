c7687700676e420fe69eb8e969720d9590b31d18
xiaoahng0323 <songxiaohang030323@163,com>
2023-07-26 12:17:12 +0800

    feat: 获取提示内容

---
+++ src/main/java/com/ps/pojo/Tip_Content.java
@@ -0,0 +1,14 @@
+package com.ps.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tip_Content {
+    private String user_auth;
+    private String stste;
+    private String tip_content;
+}
